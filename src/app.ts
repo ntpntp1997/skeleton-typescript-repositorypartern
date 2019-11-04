dfc52c5e1ba42f11981ab1eba08dae7d2650bce9
Phương nguyễn <phuong.n.t.97.08@gmail.com>
2019-11-05 01:41:56 +0700

    repository parten

--- src/app.ts
+++ src/app.ts
@@ -4,24 +4,25 @@ import Constants from './config/constants/Constants';
 import dotenv from 'dotenv';
 import * as bodyParser from 'body-parser';
 import IRouterAPI from './routes/apiRouter';
- 
+
 export class App {
   public app: express.Application;
-  public port: number;
+  public port: string;
   private route: IRouterAPI;
-  constructor(port) {
+  // tslint:disable-next-line:no-shadowed-variable
+  constructor(port: string) {
     this.app = express();
     this.port = port;
-    this.route = new IRouterAPI;
+    this.route = new IRouterAPI();
     this.initializeMiddlewares();
     this.router();
     // this.initializeControllers(controllers);
   }
- 
+
   private initializeMiddlewares() {
     this.app.use(bodyParser.json());
   }
- 
+
   private initializeControllers(controllers) {
     controllers.forEach((controller) => {
       this.app.use('/', controller.router);
@@ -32,18 +33,20 @@ export class App {
   }
   public listen() {
     this.app.listen(this.port, () => {
+      // tslint:disable-next-line:no-console
       console.log(`App listening on the port ${this.port}`);
     });
   }
 }
 dotenv.config()
 
-const port = process.env.APP_PORT;
+const port = process.env.APP_PORT || '3000';
 const app = new App(port);
 try {
-    let db = new DataAccess(new Constants());
+    const db = new DataAccess(new Constants());
     db.connect();
 } catch (error) {
+    // tslint:disable-next-line:no-console
     console.log(error)
 }
 
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -10,8 +10,31 @@ export interface User extends Document {
 const UserSchema: Schema = new Schema({
   email: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
-  lastName: { type: String, required: true }
+  lastName: { type: String, required: true },
+  username: { type: String, required: true, unique: true},
+  passwork: { type: String, required: true},
+  phone: { type: String, required: true},
+  address: { type: String, required: true},
+  avatar: { type: String, required: true},
+  role: [],
+  permiss: [],
+  createAt: { type: Number, default: Date.now},
+  updateAt: { type: Number, default: null},
+  deleteAt: { type: Number, default: null},
 });
 
+UserSchema.pre('save', function (next) {
+  this.log('saving user...');
+  next();
+});
+
+UserSchema.post('save', function (doc) {
+  this.log('user saved!');
+});
+
+UserSchema.method('log', (message) => {
+    console.log('log: ' + message);
+});
 // Export the model and return your IUser interface
 export default mongoose.model<User>('User', UserSchema);
+mongoose.model('User')
